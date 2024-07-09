d1c2a8bb3c9b592c225e4bd59af679779053c93e
maoSantaella <mao.santaella@gmail.com>
2024-07-09 15:28:26 +0000

    new dist

---
+++ actividades/story_content/triggers.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67AFZhx0zG4":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
